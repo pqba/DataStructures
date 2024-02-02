bc82e73e17e364833a1b6332d84c8028f0338110
Femi Makinde <femimakinde@icloud.com>
2024-02-02 09:42:00 -0600

    Unfinished tests for basic LL functions

--- testImplements.cpp
+++ testImplements.cpp
@@ -1,41 +1,26 @@
-#include <iostream>
-#include "Stack.h"
 #include "Data.h"
+#include <iostream>
+#include "LinkedList.h"
+#include "Stack.h"
 
 int main()
 {
-    int N = 5;
-    Stack myStack(N);
+    int N = 10;
+    LinkedList l;
     Data *list = new Data[N];
+    Node * nodes = new Node[N];
 
     //Basic testing script for Stack, to be more rigorous in future.
     for (int i = 0; i < N; i++)
     {
 
         static char n[] = {'x'};
-        Data v(i, 1.5, n);
+        Data v(i, 1.3 * i, n);
         list[i] = v;
+        nodes[i].info = v;
     }
-
-    for (int k = 0; k < N; k++)
-    {
-        *(list+k)->print();
-        myStack.stack_push(*(list + k));
-
+    for(int i = 0; i < N;i++){
+        
     }
-    std::cout << "done fill." << std::endl;
-
-    while (!myStack.isEmpty())
-    {
-        myStack.stack_print();
-        myStack.stack_pop();
-    }
-    myStack.stack_print();
-    myStack.stack_push(list[4]);
-    myStack.stack_print();
-
-    std::cout << "End program." << std::endl;
-
-    delete[] list;
     return 0;
 }
