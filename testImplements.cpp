dd7d494c99f812b9c97189df581edcf3896b5022
Femi Makinde <femimakinde@icloud.com>
2024-02-07 09:49:52 -0600

    Tests for remove and add

--- testImplements.cpp
+++ testImplements.cpp
@@ -5,12 +5,10 @@
 // Compilation:  g++ -std=c++11 -o tImpl testImplements.cpp Data.cpp LinkedList.cpp
 int main()
 {
-    int N = 10;
+    int N = 40;
     LinkedList l;
     
     for (int i = 0; i < N; ++i) {
-        std::cout << "size " << l.size() << std::endl;
-        std::cout << "content: " << l.print() << std::endl;
         // Create a Data object for each node
         Data d(i, i + 0.5, "S" + std::to_string(i)); // Example data creation
 
@@ -18,6 +16,15 @@ int main()
         // Add the current node to the linked list
         l.add(currentNode);
     } 
+    for(int i = 0; i < N; i++){
+        Node * first = l.get(0);
+        l.remove(first);
+        Data v(i*3,i,"Q");
+        Node * nd = new Node(v);
+        l.remove(nd); 
+        delete nd;
+    }
+    std::cout << l.size() << "\n"  << l.print() << std::endl;
     return 0;
 }
 
