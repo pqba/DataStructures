0fdac8c7728dab519770769358a342fee991dbcc
femiAdvanced <femimakinde@icloud.com>
2024-02-09 17:18:41 -0600

    Queue template created

--- Queue.h
+++ Queue.h
@@ -7,12 +7,24 @@ class Queue {
     private:
         Data * array;
         int head;
+        int count;
         int absolute_max = 1000;
     public:
         Queue();
+        Queue(int);
+        Queue(Data *, int);
         ~Queue();
+        
         void clear();
+        bool isEmpty();
+        int size();
+
         void enqueue(Data);
         Data dequeue();
+
+        std::string print();
+        void checkIndex(std::string,int);
+        int boundCapacity(int);
+
 };
 #endif
--- testImplements.cpp
+++ testImplements.cpp
@@ -1,28 +1,12 @@
 #include "Data.h"
 #include <iostream>
-#include "LinkedList.h"
+#include "Queue.h"
 
 // Compilation:  g++ -std=c++11 -o tImpl testImplements.cpp Data.cpp LinkedList.cpp
 
 int main()
 {
-    int N = 10;
-    LinkedList l;
-    for (int i = 0; i < N; ++i) {
-        // Create a Data object for each node
-        Data d(i, i + 0.5, "S" + std::to_string(i)); // Example data creation (int, float, string)
-        Node * currentNode = new Node (d);
-        // Add the current node to the linked list
-        l.add(currentNode);
-    } 
-    std::cout << l.print() << std::endl;
-    for(int i = 0; i < N/2; i++){
-        Data nD(i*N, i+ 1, "N" + std::to_string(i));
-        Node *  betterNode = new Node(nD);
-        l.set(betterNode,i);
-    }
 
-    std::cout << l.print() << std::endl;
     
     return 0;
 }
