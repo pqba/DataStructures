9a9a5c804bfacd4561e2c6dec609900c0a512429
Femi Makinde <femimakinde@icloud.com>
2024-02-25 17:23:09 -0600

    Unworking cpp files for tests on structures

---
+++ test/Structures/dllTest.cpp
@@ -0,0 +1,18 @@
+#include "../../include/Structures/Data.h"
+#include "../../include/Structures/DoublyLinkedList.h"
+#include <iostream>
+#include <random>
+
+// UNFINISHED, NOT IMPLEMENTED YET.
+
+void runTests();
+
+
+void basicTests(){
+    int i = 5;
+} 
+
+
+void runTests(){
+    basicTests();
+} 
