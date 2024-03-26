9245fa8d21795a3ad999f80e064b6230cc3f0737
Femi Makinde <femimakinde@icloud.com>
2024-03-26 10:55:10 -0500

    Removed comment description

--- src/Structures/Stack.cpp
+++ src/Structures/Stack.cpp
@@ -5,16 +5,6 @@
 #include <string>
 
 /*
-ADT
-A stack, S of type T is a sequence of items of type T on which the following operations are defined:
-LIFO: Last put into the stack is the first to be popped off
-
-Initialize the stack S to be the empty stack
-Determine whether or not the stack S is empty()
-Determine whether or not the stack S is full()
-push() a new item of type T onto the top of the stack, S
-If S in not empty, pop() an item from the top of the stack, S.
-
 References : https://www.cse.unr.edu/~sushil/class/cs202/notes/stacks/stacks.html
 */
 
