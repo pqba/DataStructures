ffbe0002d5b2a0389bdd428fa5380196b5c3eb00
Femi Makinde <femimakinde@icloud.com>
2024-04-07 23:23:52 -0500

    Changed to i32, updated display funcs

--- include/Common/MusicPlayer.h
+++ include/Common/MusicPlayer.h
@@ -4,21 +4,38 @@
 #include "../Structures/DoublyLinkedList.h"
 #include <string>
 
+
 // Song struct with title, artist, and length
 struct Song {
     std::string title;
     std::string musician;
     std::string albumCover;
-    uint16_t length;
-    Song(std::string t,std::string mus,std::string ac,uint16_t len) {
+    int32_t length;
+    Song(std::string t,std::string mus,std::string ac,int32_t len) {
         title = t; musician = mus; length = len; albumCover = ac;
     }
     ~Song(){
         title = musician = albumCover = "N/A";
         length = 0;
     }
-    std::string display() {
-        return title + "|" + musician + "|" + std::to_string(length) + "\n" + albumCover;
+    std::string songLength() const {
+        int32_t minutes = length % 60;
+        int32_t seconds = length / 60;
+        std::string minString = std::to_string(minutes);
+        std::string secString = std::to_string(seconds); 
+        if(minutes < 10){
+            minString = "0" + minString;
+        }
+        if(seconds < 10){
+            secString = "0" + secString;
+        }
+        return minString + ":" + secString;
+    }   
+    std::string info() const {
+        return title + " | " + musician + " | " + songLength() + "\n";
+    }
+    std::string display() const {
+        return info() + albumCover;
     }
     bool operator==(const Song& other){
         return other.title == title && other.musician == musician && other.albumCover == albumCover && other.length == length;
@@ -44,7 +61,12 @@ class MusicPlayer {
         void addSong(Song);
         void removeSong(Song);
 
+        void buildSong(int);
+        void buildPlaylist();
+
         void display();
+        void displayVisualizer(const Song&);
 
 };
+
 #endif
